a4c39f4e0b05ca28a221e7f29c78542fc76de126
Giannis Christofakis <giannis_christofakis@hotmail.com>
2020-01-10 19:21:02 +0200

    Set db values to .env file. Insert Spatie dependency. Config database connection file. Create permission,leaves,users migration files.

---
+++ database/migrations/2020_01_10_151217_create_leaves_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeavesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leaves', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('reason');
+            $table->dateTime('submit');
+            $table->date('vacation_start');
+            $table->date('vacation_end');
+            $table->enum('status', ['pending','approved','rejected']);
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leaves');
+    }
+}
+
